7b8f4296853b083dedcf195b5af5e95214618f30
ricou12 <>
2023-07-20 07:07:30 +0200

    compilation de l'app en war

--- src/main/java/com/api/mushroom/Application.java
+++ src/main/java/com/api/mushroom/Application.java
@@ -3,9 +3,11 @@ package com.api.mushroom;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application extends SpringBootServletInitializer implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -13,6 +15,12 @@ public class Application implements CommandLineRunner {
 
 	public void run(String... args) throws Exception {
  		System.out.println("L'application est démarré !");
+	}
+
+	//
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
 	}
 
 }
